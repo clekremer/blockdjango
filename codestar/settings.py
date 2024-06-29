27441543058f449d27bc11061c2ec6771cc3aa0c
clemens kremer <molkrich@gmx.de>
2024-06-29 17:15:53 +0000

    deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['djblogdep.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['blockdjango.herokuapp.com', 'localhost']
 
 
 # Application definition
